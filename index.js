ca775ee6e38e50c8c96e72e59421f2c0c2667959
DILIP KUMAR BC <dilipkumarbc8055@gmail.com>
2024-05-27 18:05:44 +0530

    done

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ import path from "path";
 const server = express();
 server.use(cors());
 server.use(bodyParser.json()); // Use bodyParser.json() to parse JSON bodies
-
+const port = process.env.PORT || 4000;
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 server.set("views", path.join(path.resolve(), "src", "views"));
 server.set("view engine", "ejs");
 server.use("/", messageRoute);
 
-server.listen(3000, async () => {
+server.listen(port, async () => {
   // Make the callback async
   await connectionToDB(); // Await connectionToDB() to ensure database connection
-  console.log("Server is listening at port 3000");
+  console.log("Server is listening at port ", port);
 });
